b4d688921b5f868cc7b3c9441324ce8038501600
Terry Parker <terry.parker@thoughtworks.com>
2023-02-06 12:23:31 -0700

    added create plateau and create rover functionality

--- src/marsRover.js
+++ src/marsRover.js
@@ -1,7 +1,148 @@
-console.log();
+
+
+//console.log();
+
+//const fs = require("fs");
+const TESTFILE = '/Users/terryparker/Learning/mars-rover/src/input.txt';
 
 const run = () => {
   console.log("Welcome to Mars!");
+
+  let data = readInputData(TESTFILE);
+
+  let plateau = new CreatePlateau(data);
+  let numRovers = howManyRovers(data);
+  let rovers = [];
+
+  for (let i = 1; i <= numRovers*2; i+=2){
+    let tmpRover = new CreateRover(data, plateau, i);
+    rovers.push(tmpRover);
+
+  }
+
+  // for (let i = 0; i < rovers.length; i++){
+  //   rovers[i].moveRover();
+  // }
+
+let x = 1;
+
+
+
+
+
+  //todo: read input
+  //todo: create plateau
+  //todo: create rovers
+  //todo: move rover1, repeat for each rover
+  //todo: report final location
+
+
+
+
 };
 
 run();
+
+
+function readInputData(filePath){
+  let fs = require('fs');
+  let data = fs.readFileSync(filePath, 'utf8');
+
+  return data;
+  // let inputSplit = data.toString().trim().split('\n');
+};
+
+
+
+function howManyRovers(data){
+  let dataSplit = data.toString().trim().split('\n');
+  let ds2 = dataSplit[0];
+  let numRovers = (dataSplit.length -1) /2;
+  return numRovers;
+}
+
+
+function CreatePlateau(input){
+  this.maxX = plateauGetMaxX(input);
+  this.maxY = plateauGetMaxY(input);
+
+  function getMaxX(){
+    return this.maxX;
+  }
+  function getMaxY(){
+    return this.maxY;
+  }
+
+  function plateauGetMaxX(input){
+    let firstLine = input.split('\n',1);
+    return parseInt(firstLine.toString().split(' ')[0]);
+  }
+  function plateauGetMaxY(input){
+    let firstLine = input.split('\n',1);
+    return parseInt(firstLine.toString().split(' ')[1]);
+  }
+}
+
+
+
+
+
+function CreateRover(input, plateau, roverNumber){
+  this.plateauMaxX = plateau.maxX; //plateauGetMaxX(input);
+  this.plateauMaxY = plateau.maxY; //plateauGetMaxY(input);
+  this.x = getXCoordinate(input, roverNumber);
+  this.y = getYCoordinate(input, roverNumber);
+  this.orientation = getOrientation(input, roverNumber);
+  this.moveInstructions = getMoveInstructions(input, roverNumber);
+//  this.moveRover = moveRover1();
+
+
+  function getXCoordinate(input, roverNumber){
+    // let tmp1 = input.split('\n');
+    // let tmp2 = tmp1[roverNumber].split(' ');
+    let tmp2 = splitToRoverPosition(input,roverNumber);
+    let tmp3 = tmp2[0].toString();
+
+    return(parseInt(tmp3));
+//    return 1;
+  }
+
+  function getYCoordinate(input, roverNumber){
+    // let tmp1 = input.split('\n');
+    // let tmp2 = tmp1[roverNumber].split(' ');
+    let tmp2 = splitToRoverPosition(input, roverNumber);
+    let tmp3 = tmp2[1].toString();
+
+    return(parseInt(tmp3));
+  }
+
+  function getOrientation(input, roverNumber){
+    // let tmp1 = input.split('\n');
+    // let tmp2 = tmp1[roverNumber].split(' ');
+    let tmp2 = splitToRoverPosition(input, roverNumber);
+    let tmp3 = tmp2[2].toString();
+
+    return(tmp3);
+  }
+
+  function splitToRoverPosition(input, roverNumber){
+    return (input.split('\n')[roverNumber].split(' '));
+  }
+
+  function getMoveInstructions(input, roverNumber){
+    let tmp1 = input.split('\n');
+    let tmp2 = tmp1[roverNumber+1].split(' ');
+    let tmp3 = tmp2[0].toString();
+
+    return(tmp3);
+  }
+
+  function moveRover1(){
+    let x = 'in moveRover';
+  }
+}
+
+
+
+module.exports = {CreatePlateau,
+    CreateRover };
--- src/marsRover.test.js
+++ src/marsRover.test.js
@@ -1,3 +1,90 @@
+
+const { CreatePlateau } = require("./marsRover");
+const { CreateRover } = require("./marsRover");
+const { CreateGame } = require("./marsRover")
+require("./marsRover.js");
+
+
+
 describe("marsRover", () => {
+
+  const INPUTDATA ='5 5\n1 3 N\nLMLMLMMM'
+  const PLATEAUX = '5';
+  const PLATEAUY = '5';
+  const ROVERX = '1';
+  const ROVERY = '3';
+  const ROVERORIENTATION = 'N';
+  const ROVERMOVES = 'LMLMLMMM';
+  const TESTFILE1 = '/Users/terryparker/Learning/mars-rover/src/input.txt';
+
   it.todo("should do something really neat");
+
+  it("should do something", () =>{
+    const value = 55;
+
+    expect(value).toEqual(55);
+  });
+
+
+
+  it("should read input file and return it as a string", () => {
+
+    let inputData = readInputData(TESTFILE1);
+
+    expect (inputData).toEqual('5 5\n1 3\nLMLMLMM');
+  });
+
+
+
+  it("should create plateau and return plateau maximum x and y coordinates", () => {
+    const input = INPUTDATA;
+
+    const plateau = new CreatePlateau(input);
+
+    expect(plateau.maxX).toEqual(5);
+    expect (plateau.maxY).toEqual(5);
+
+  });
+
+  it ("should return number of rovers to be used", () => {
+    const input = INPUTDATA;
+    expect(howManyRovers(input).toEqual(1));
+  });
+
+  it("should create a rover object and set initial x & y coordinates", () => {
+    const input = INPUTDATA;
+    const plateau = new CreatePlateau(input);
+
+    const rover = new CreateRover(input, plateau, 1);
+
+    expect(rover.x).toEqual(1);
+    expect(rover.y).toEqual(3);
+    expect(rover.orientation).toEqual('N');
+    expect(rover.moveInstructions).toEqual('LMLMLMMM')
+
+  });
+
+  it("should create a rover object and set orientation", () => {
+    const input = INPUTDATA;
+    const plateau = new CreatePlateau(input);
+
+    const rover = new CreateRover(input, plateau, 1);
+
+    expect(rover.orientation).toEqual('N');
+
+  });
+
+  it("should create a rover object and set instructions", () => {
+    const input = INPUTDATA;
+    const plateau = new CreatePlateau(input);
+
+    const rover = new CreateRover(input, plateau, 1);
+
+    expect(rover.moveInstructions).toEqual('LMLMLMMM')
+
+  });
+
+
 });
+
+
