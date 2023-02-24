db92285b774d412e337ecafebb64566f022a6b48
Terry Parker <terry.parker@thoughtworks.com>
2023-02-24 13:36:29 -0700

    added 45 degree turns and moves

--- src/marsRover.js
+++ src/marsRover.js
@@ -7,10 +7,11 @@
 //done: report final location
 //done: move rover backwards
 //done: edge detection.  and what do we do when we reach an edge
-//todo: multiple rovers.  push it out of the way
-//todo: rotate through the rovers executing 1 instruction each rotation
+//done: 45 degree turns. 'l' and 'r'.  fix moves for this too.
+//todo: multiple rovers at same coordinates.  moving rover to push stationary rover out of the way
+//todo: rotate through the rovers executing 1 instruction round robin style
 
-
+let fs = require('fs');
 
 const TESTFILE = '/Users/terryparker/Learning/mars-rover/src/input.txt';
 
@@ -30,9 +31,11 @@ const run = () => {
 run();
 
 
+
 function readInputData(filePath){
-  let fs = require('fs');
+
   let data = fs.readFileSync(filePath, 'utf8');
+
 
   return data;
 };
@@ -45,6 +48,8 @@ function howManyRovers(data){
   let numRovers = (dataSplit.length -1) /2;
   return numRovers;
 }
+
+
 
 
 function CreatePlateau(input){
@@ -67,8 +72,10 @@ function CreatePlateau(input){
     return parseInt(firstLine.toString().split(' ')[1]);
   }
 
-  return (getMaxX, getMaxY, this.maxX, this.maxY);
+//  return (getMaxX, getMaxY, this.maxX, this.maxY);  // may not need the return. research
 }
+
+
 
 
 
@@ -146,73 +153,163 @@ function CreateRover(input, plateau, roverNumber){
    }
  }
 
-function moveRover(rover){
-    let instructions = rover.moveInstructions.split('');
+function moveRover(rover) {
+  let instructions = rover.moveInstructions.split('');
 
-    for (let value of instructions) {
-      if (rover.atEdge)
-       return
-      if (value === 'L') {
-        rover.orientation = turnLeft(rover.orientation);
-      }
-      if (value === 'R') {
-        rover.orientation = turnRight(rover.orientation);
-      }
-      if (value === 'M') {
-        moveForward(rover);
-      }
-      if (value === 'B'){
-        moveBackward(rover);
-      }
+  for (let value of instructions) {
+    if (rover.atEdge)
+      return
+    if (value === 'L') {
+//      turnLeft(rover);
+      rover.orientation = turnLeft(rover.orientation);
+    }
+    if (value === 'l'){
+      rover.orientation = turnLeft45(rover.orientation);
+    }
+    if (value === 'R') {
+      rover.orientation = turnRight(rover.orientation);
+    }
+    if (value === 'r')
+      rover.orientation = turnRight45(rover.orientation);
+    if (value === 'M') {
+      moveForward(rover);
+    }
+    if (value === 'B') {
+      moveBackward(rover);
+    }
+
   }
 
   function turnLeft(direction) {
-    if (direction === 'N')        return 'W';
-    else if (direction === 'W')   return 'S';
-    else if (direction === 'S')   return 'E';
-    else if (direction === 'E')   return 'N';
+//  function turnLeft(rover){
+//    let orientation = rover.orientation;
+//    if (orientation === 'N') rover.orientation = 'W';
+//     if (orientation === 'W') rover.orientation = 'S';
+//     if (orientation === 'S') rover.orientation = 'E';
+//     if (orientation === 'E') rover.orientation = 'N';
+//     if (direction === 'N') // rover.orientation = 'W';
+    if (direction === 'N')   return 'W';
+    else if (direction === 'NW') return 'SW';
+    else if (direction === 'W') return 'S';
+    else if (direction === 'SW') return 'SE';
+    else if (direction === 'S') return 'E';
+    else if (direction === 'SE') return 'NE';
+    else if (direction === 'E') return 'N';
+  }
+
+  function turnLeft45(direction) {
+//  function turnLeft(rover){
+//    let orientation = rover.orientation;
+//    if (orientation === 'N') rover.orientation = 'W';
+//     if (orientation === 'W') rover.orientation = 'S';
+//     if (orientation === 'S') rover.orientation = 'E';
+//     if (orientation === 'E') rover.orientation = 'N';
+//     if (direction === 'N') // rover.orientation = 'W';
+    if (direction === 'N') return 'NW';
+    else if (direction === 'NW') return 'W';
+    else if (direction === 'W') return 'SW';
+    else if (direction === 'SW') return 'S';
+    else if (direction === 'S') return 'SE';
+    else if (direction === 'SE') return 'E';
+    else if (direction === 'E') return 'NE';
+    else if (direction === 'NE') return 'N';
   }
 
   function turnRight(direction) {
-    if (direction === 'N')        return 'E';
-    else if (direction === 'W')   return 'N';
-    else if (direction === 'S')   return 'W';
-    else if (direction === 'E')   return 'S';
+    if (direction === 'N') return 'E';
+    else if (direction === 'NE') return 'SE';
+    else if (direction === 'W') return 'N';
+    else if (direction === 'NW') return'NE';
+    else if (direction === 'S') return 'W';
+    else if (direction === 'SW') return 'NW';
+    else if (direction === 'E') return 'S';
+    else if (direction === 'SE') return 'SW';
+  }
+
+  function turnRight45(direction) {
+    if (direction === 'N') return 'NE';
+    else if (direction === 'NE') return 'E';
+    else if (direction === 'W') return 'NW';
+    else if (direction === 'NW') return 'N';
+    else if (direction === 'S') return 'SW';
+    else if (direction === 'SW') return 'W';
+    else if (direction === 'E') return 'SE';
+    else if (direction === 'SE') return 'S';
   }
 
   function moveForward(rover) {
     if (rover.orientation === 'N') {
       if (rover.y === rover.plateauMaxY) {
+//        moveOne(rover, [0, 1]);  // use this to
         rover.y += 1;
         rover.atEdge = true;
       } else {
         rover.y += 1
       }
-    }
-    else if (rover.orientation === 'S') {
+    } else if (rover.orientation === 'S') {
       if (rover.y === 0) {
         rover.y -= 1;
         rover.atEdge = true;
       } else {
         rover.y -= 1
       }
-    }
-    else if (rover.orientation === 'W') {
-      if (rover.x === 0){
+    } else if (rover.orientation === 'W') {
+      if (rover.x === 0) {
         rover.x -= 1;
         rover.atEdge = true;
+      } else {
+        rover.x -= 1
       }
-      else { rover.x -= 1}
-    }
-    else if (rover.orientation === 'E'){
+    } else if (rover.orientation === 'E') {
       if (rover.x === rover.plateauMaxX) {
         rover.x += 1;
         rover.atEdge = true;
       } else {
         rover.x += 1
       }
+    } else if (rover.orientation === 'NE') {
+      if (rover.x === rover.plateauMaxX || rover.y === rover.plateauMaxY) {
+        rover.x += 1;
+        rover.y += 1;
+        rover.atEdge = true;
+      } else {
+        rover.x += 1;
+        rover.y += 1;
+      }
+    } else if (rover.orientation === 'NW') {
+      if (rover.x === 0 || rover.y === rover.plateauMaxY ) {
+        rover.x -= 1;
+        rover.y += 1;
+        rover.atEdge = true;
+      } else {
+        rover.x -= 1;
+        rover.y += 1;
+      }
+    } else if (rover.orientation === 'SE') {
+      if (rover.x === rover.plateauMaxX || rover.y === 0 ) {
+        rover.x += 1;
+        rover.y -= 1;
+        rover.atEdge = true;
+      } else {
+        rover.x += 1;
+        rover.y -= 1;
+      }
+    } else if (rover.orientation === 'SW') {
+      if (rover.x === 0 || rover.y === 0) {
+        rover.x -= 1;
+        rover.y -= 1;
+        rover.atEdge = true;
+      } else {
+        rover.x -= 1;
+        rover.y -= 1;
+      }
     }
   }
+  function moveOne(rover, coord){
+    rover.y += coord[1];
+    rover.x += coord[0];
+  }
+
 
   function moveBackward(rover) {
     if (rover.orientation === 'N') rover.y -= 1;
@@ -234,9 +331,11 @@ function moveRover(rover){
 }
 
 function reportRovers(rovers){
+  let finalReport = '';
   for (let i = 0; i < rovers.length; i++){
-    report(rovers[i]);
+    finalReport += report(rovers[i]) + '\n';
   }
+  return finalReport;
 }
 
 
@@ -258,7 +357,8 @@ function report(rover){
 
 
 module.exports = {CreatePlateau,
-    CreateRover, readInputData, howManyRovers, moveRover, report };
+    CreateRover, createRoversArray,
+  readInputData, howManyRovers, moveRovers, moveRover, reportRovers, report };
 
 
 
--- src/marsRover.test.js
+++ src/marsRover.test.js
@@ -1,5 +1,6 @@
 
-const { CreatePlateau, CreateRover, readInputData, howManyRovers, moveRover, report } = require("./marsRover");
+const { CreatePlateau, CreateRover, createRoversArray, readInputData,
+  howManyRovers, moveRovers, moveRover, reportRovers, report } = require("./marsRover");
 // const { CreateRover } = require("./marsRover");
 
 
@@ -18,14 +19,23 @@ describe("marsRover", () => {
   const ROVERMOVES = 'LMLMLMMM';
   const TESTFILE1 = '/Users/terryparker/Learning/mars-rover/src/test.txt';
 
-  it.todo("should do something really neat");
+  // it.todo("should do something really neat");
+  //
+  // it("should do something", () =>{
+  //   const value = 55;
+  //   expect(value).toEqual(55);
+  // });
 
-  it("should do something", () =>{
-    const value = 55;
-    expect(value).toEqual(55);
-  });
+it("should do full end to end", () => {
+  let inputData = readInputData(TESTFILE1);
+  let plateau = new CreatePlateau(inputData);
+  let rovers = createRoversArray(inputData, plateau);
+  moveRovers(rovers);
+  let finalReport = reportRovers(rovers);
 
+  expect(finalReport).toEqual("3 2 E\n");
 
+})
 
   it("should read input file and return it as a string", () => {
 
@@ -35,13 +45,13 @@ describe("marsRover", () => {
   });
 
   it ("should return number of rovers to be used", () => {
-    const input = INPUTDATA;  //'5 5\n1 3 N\nLMLMLMMM'
+    const input = '5 5\n1 3 N\nLMLMLMMM';  //'5 5\n1 3 N\nLMLMLMMM'
     expect(howManyRovers(input)).toEqual(1);
   });
 
 
   it("should create plateau and return plateau maximum x and y coordinates", () => {
-    const input = INPUTDATA;
+    const input = '5 5\n1 3 N\nLMLMLMMM';
     const plateau = new CreatePlateau(input);
 
     expect(plateau.maxX).toEqual(5);
@@ -52,7 +62,7 @@ describe("marsRover", () => {
 
 
   it("should create a rover object and set initial x & y coordinates", () => {
-    const input = INPUTDATA;
+    const input = '5 5\n1 3 N\nLMLMLMMM';
     const plateau = new CreatePlateau(input);
     const rover = new CreateRover(input, plateau, 1);
 
@@ -62,7 +72,7 @@ describe("marsRover", () => {
   });
 
   it("should create a rover object and set orientation", () => {
-    const input = INPUTDATA;
+    const input = '5 5\n1 3 N\nLMLMLMMM';
     const plateau = new CreatePlateau(input);
     const rover = new CreateRover(input, plateau, 1);
 
@@ -71,7 +81,7 @@ describe("marsRover", () => {
   });
 
   it("should create a rover object and set instructions", () => {
-    const input = INPUTDATA;
+    const input = '5 5\n1 3 N\nLMLMLMMM';
     const plateau = new CreatePlateau(input);
     const rover = new CreateRover(input, plateau, 1);
     const roverX = rover.getXCoord(input, 1);
@@ -197,6 +207,51 @@ describe("marsRover", () => {
 
     expect(report(rover)).toEqual('+5 1 E');
   });
+
+  it("should turn rover 45 degrees left when a lower case 'l' is the command", () => {
+    const input = '5 5\n2 2 NE\nl';
+    const plateau = new CreatePlateau(input);
+    const rover = new CreateRover(input, plateau, 1);
+    moveRover(rover);
+
+    expect(report(rover)). toEqual('2 2 N');
+  });
+
+  it("should turn rover 45 degrees RIGHT when a lower case 'r' is the command", () => {
+    const input = '5 5\n2 2 E\nr';
+    const plateau = new CreatePlateau(input);
+    const rover = new CreateRover(input, plateau, 1);
+    moveRover(rover);
+
+    expect(report(rover)). toEqual('2 2 SE');
+  });
+
+  it("should move rover at 45 degrees to new location", () => {
+    const input = '5 5\n2 2 NE\nM';
+    const plateau = new CreatePlateau(input);
+    const rover = new CreateRover(input, plateau, 1);
+    moveRover(rover);
+
+    expect(report(rover)). toEqual('3 3 NE');
+  });
+
+  it("should rotate right 45 degrees 4 times", () => {
+    const input = '5 5\n2 2 N\nrrrr';
+    const plateau = new CreatePlateau(input);
+    const rover = new CreateRover(input, plateau, 1);
+    moveRover(rover);
+
+    expect(report(rover)). toEqual('2 2 S');
+  })
+
+  it("should move around the board.  0 0 to 5 5", () => {
+    const input = '5 5\n0 0 N\nRMMMMMLMMMMMlllMMMMM';
+    const plateau = new CreatePlateau(input);
+    const rover = new CreateRover(input, plateau, 1);
+    moveRover(rover);
+
+    expect(report(rover)). toEqual('0 0 SW');
+  })
 
 });
 
